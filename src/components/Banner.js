fe0a78ec6d45f48229b8df561e1cc1fc82760922
Damon Tian <damontian@live.com>
2018-01-08 15:45:36 -0500

    added dyanmic resume link

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -10,7 +10,7 @@ class Banner extends React.Component {
               <hr/>
               <h4 style={{maxWidth: "90%", margin: "0 auto", padding: "0 0 1em", lineHeight: "25px"}}><strong>My Top Skills:</strong> HTML/CSS, JavaScript, React.js, Node.js/Express.js, Ruby, Ruby on Rails, PostgreSQL, WordPress, Git/Github, Bootstrap </h4>
               <div>
-                <a className="btn btn-default" href="https://www.dropbox.com/s/ptqq5goeer7v0j8/damon-tian-resume-v3.pdf?dl=0">RESUME</a>
+                <a className="btn btn-default" target="_blank" href="https://drive.google.com/file/d/17hPBvj_j4x9EQ9xdu-npg9hrNG9VKUA4/view">RESUME</a>
                 <a className="btn btn-default" href="#footer" style={{marginLeft: "1em"}}>SEND A MESSAGE</a>
               </div>
 
