aa23417cd83264aa6b3d3706fb3acdbf483d0c76
Damon Tian <damontian@live.com>
2019-10-28 01:48:52 -0400

    updated 4 10.28.19

--- index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./src/components/App";
-import "./style.scss";
-
-ReactDOM.render(<App />, document.getElementById("root"));
