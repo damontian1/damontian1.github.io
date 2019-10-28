d6592336d8b8cea15e34ed6103fc18c57c78c92e
Damon Tian <damontian@live.com>
2019-10-28 01:47:49 -0400

    updated 3 10.28.19

--- src/dist/src.e31bb0bc.js
+++ src/dist/src.e31bb0bc.js
@@ -32632,7 +32632,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59490" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60723" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
