fc6cce2f9457dc04af6f24981d5ebf96b5b9b7e5
Damon Tian <damontian@live.com>
2021-05-10 01:32:59 -0400

    fix: removed og graph and twitter seo

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,51 +12,6 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'I’m Damon, a Web Developer from New York City and I love building beautiful and intuitive web applications.' },
-      { property: "og:site_name", content: "Damon Tian | Portfolio" },
-      { hid: "og:type", property: "og:type", content: "website" },
-      {
-        hid: "og:url",
-        property: "og:url",
-        content: "http://damontian1.github.io",
-      },
-      {
-        hid: "og:title",
-        property: "og:title",
-        content: "Damon Tian | Portfolio",
-      },
-      {
-        hid: "og:description",
-        property: "og:description",
-        content: "I’m Damon, a Web Developer from New York City and I love building beautiful and intuitive web applications.",
-      },
-      {
-        hid: "og:image",
-        property: "og:image",
-        content: "https://damontian1.github.io/seo-card.png",
-      },
-      { property: "og:image:width", content: "740" },
-      { property: "og:image:height", content: "300" },
-      { name: "twitter:card", content: "summary_large_image" },
-      {
-        hid: "twitter:url",
-        name: "twitter:url",
-        content: "https://damontian1.github.io",
-      },
-      {
-        hid: "twitter:title",
-        name: "twitter:title",
-        content: "Damon Tian | Portfolio",
-      },
-      {
-        hid: "twitter:description",
-        name: "twitter:description",
-        content: "I’m Damon, a Web Developer from New York City and I love building beautiful and intuitive web applications.",
-      },
-      {
-        hid: "twitter:image",
-        name: "twitter:image",
-        content: "https://damontian1.github.io/seo-card.png",
-      },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
